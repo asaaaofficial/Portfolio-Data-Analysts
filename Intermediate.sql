57044b42be6ef78de1044161a537c6c6c48ab1ec
asaaaofficial <asaaaofficial@gmail.com>
2025-03-03 05:51:57 +0700

    Update Intermediate.sql

--- Intermediate.sql
+++ Intermediate.sql
@@ -2,27 +2,27 @@
 -- Joins
 
 SELECT *
-FROM parks_and_recreation.employee_demographics;
+FROM employee_demographics;
 
 SELECT *
-FROM parks_and_recreation.employee_salary;
+FROM employee_salary;
 
 SELECT *
-FROM parks_and_recreation.employee_demographics AS dem
-INNER JOIN parks_and_recreation.employee_salary AS sal
-	ON parks_and_recreation.dem.employee_id = parks_and_recreation.sal.employee_id
+FROM employee_demographics AS dem
+INNER JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
 ;
 
 SELECT *
-FROM parks_and_recreation.employee_demographics AS dem
-RIGHT JOIN parks_and_recreation.employee_salary AS sal
-	ON parks_and_recreation.dem.employee_id = parks_and_recreation.sal.employee_id
+FROM employee_demographics AS dem
+RIGHT JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
 ;
 
 -- Self Join
 SELECT *
-FROM parks_and_recreation.employee_salary emp1
-JOIN parks_and_recreation.employee_salary emp2
+FROM employee_salary emp1
+JOIN employee_salary emp2
 ON emp1.employee_id = emp2.employee_id
 ;
 
@@ -32,40 +32,40 @@ emp1.last_name AS last_name_santa,
 emp2.employee_id AS emp_santa,
 emp2.first_name AS first_name_santa,
 emp2.last_name AS last_name_santa
-FROM parks_and_recreation.employee_salary emp1
-JOIN parks_and_recreation.employee_salary emp2
+FROM employee_salary emp1
+JOIN employee_salary emp2
 	ON emp1.employee_id + 1 = emp2.employee_id
 ;
 
 -- Joining multiple tables together
 SELECT *
-FROM parks_and_recreation.employee_demographics AS dem
-INNER JOIN parks_and_recreation.employee_salary AS sal
-	ON parks_and_recreation.dem.employee_id = parks_and_recreation.sal.employee_id
-    INNER JOIN parks_and_recreation.parks_departments pd
+FROM employee_demographics AS dem
+INNER JOIN employee_salary AS sal
+	ON dem.employee_id = parks_and_recreation.sal.employee_id
+    INNER JOIN parks_departments pd
     ON sal.dept_id = pd.department_id
 ;
 
 SELECT *
-FROM parks_and_recreation.parks_departments;
+FROM parks_departments;
 
 -- Unions
 SELECT age, gender
-FROM parks_and_recreation.employee_demographics
+FROM employee_demographics
 UNION ALL
 SELECT first_name, last_name
-FROM parks_and_recreation.employee_salary;
+FROM employee_salary;
 
 SELECT first_name, last_name, 'Old Man' AS Label
-FROM parks_and_recreation.employee_demographics
+FROM employee_demographics
 WHERE age > 40 AND gender = 'Male'
 UNION
 SELECT first_name, last_name, 'Old Lady' AS Label
-FROM parks_and_recreation.employee_demographics
+FROM employee_demographics
 WHERE age > 40 AND gender = 'Female'
 UNION
 SELECT first_name, last_name, 'Highly Paid Employee' AS Label
-FROM parks_and_recreation.employee_salary
+FROM employee_salary
 WHERE salary > 70000
 ORDER BY first_name, last_name
 
@@ -74,7 +74,7 @@ ORDER BY first_name, last_name
 SELECT LENGTH('sky');
 
 SELECT first_name, LENGTH(first_name)
-FROM parks_and_recreation.employee_demographics
+FROM employee_demographics
 ORDER BY 2;
 
 -- UPPER dan LOWER
@@ -82,7 +82,7 @@ SELECT UPPER('sky');
 SELECT LOWER('SKY');
 
 SELECT first_name, UPPER(first_name)
-FROM parks_and_recreation.employee_demographics
+FROM employee_demographics
 ORDER BY 2;
 
 -- Trim
@@ -95,22 +95,22 @@ RIGHT(first_name, 4),
 SUBSTRING(first_name, 3, 2),
 birth_date,
 SUBSTRING(birth_date, 3, 2)
-FROM parks_and_recreation.employee_demographics;
+FROM employee_demographics;
 
 -- Replace
 SELECT first_name, REPLACE (first_name, 'a', 'z')
-FROM parks_and_recreation.employee_demographics;
+FROM employee_demographics;
 
 -- Locate
 SELECT LOCATE('x', 'Alexander');
 
 SELECT first_name, LOCATE('An', first_name)
-FROM parks_and_recreation.employee_demographics;
+FROM employee_demographics;
 
 -- Concatenation
 SELECT first_name, last_name,
 CONCAT(first_name, ' ', last_name) AS full_name
-FROM parks_and_recreation.employee_demographics;
+FROM employee_demographics;
 
 -- Case statements
 SELECT first_name, 
@@ -118,7 +118,7 @@ last_name,
 CASE
 	WHEN age <= 30 THEN 'Young'
 END  
-FROM parks_and_recreation.employee_demographics;  
+FROM employee_demographics;  
 
 SELECT first_name, 
 last_name,
@@ -128,7 +128,7 @@ CASE
     WHEN age BETWEEN 31 and 50 THEN 'Old'
     WHEN age >= 50 THEN 'On Death`s Door'
 END AS Age_Bracket
-FROM parks_and_recreation.employee_demographics;
+FROM employee_demographics;
 
 -- Pay Increase and Bonus
 -- < 50000 = 5%
@@ -143,4 +143,4 @@ END AS New_Salary,
 CASE
 	WHEN dept_id = 6 THEN salary * .10
 END AS Bonus
-FROM parks_and_recreation.employee_salary;
+FROM employee_salary;
