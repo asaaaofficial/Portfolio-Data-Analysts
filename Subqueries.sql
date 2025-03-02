047a952464ff149924c3fd8f325934f4263333d4
asaaaofficial <asaaaofficial@gmail.com>
2025-03-03 05:53:11 +0700

    Update Subqueries.sql

--- Subqueries.sql
+++ Subqueries.sql
@@ -1,20 +1,20 @@
  -- Subqueries
 SELECT * 
-FROM parks_and_recreation.employee_demographics
+FROM employee_demographics
 WHERE employee_id IN 
 		(SELECT employee_id
-        FROM parks_and_recreation.employee_salary
+        FROM employee_salary
         WHERE dept_id = 1);
         
 SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
-FROM parks_and_recreation.employee_demographics
+FROM employee_demographics
 GROUP BY gender
 ;
         
 SELECT gender, AVG('MAX(age)')
 FROM 
 (SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
-FROM parks_and_recreation.employee_demographics
+FROM employee_demographics
 GROUP BY gender) AS Agg_table;
 
 SELECT AVG(max_age)
@@ -24,5 +24,5 @@ AVG(age) AS avg_age,
 MAX(age) AS max_age, 
 MIN(age) AS min_age, 
 COUNT(age)
-FROM parks_and_recreation.employee_demographics
+FROM employee_demographics
 GROUP BY gender) AS Agg_table;
